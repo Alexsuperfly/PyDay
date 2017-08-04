e57f33d72d7d9cc809cbef088e1a730c5f08ad39
pickerj <pickerj@live.com>
2017-08-04 14:27:16 -0400

    Added advanced menu system

--- pyday.py
+++ pyday.py
@@ -2,3 +2,20 @@
     PyDay Main Module
     Module launches main menu, then handles transitions between levels.
 """
+import pygame
+import Level1
+import Level2
+import Level3
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    
+    print("LAUNCHING ULTRA ADVANCED MAIN MENU LEVEL 5 v.03")
+    
+    Level1.main(800, 600, screen)
+    Level2.game_loop(screen)
+    Level3.Game().main(screen)
+    
+    print("Thanks for playing :)")
+
